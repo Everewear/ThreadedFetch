40deb0f3699d0993302b1cf506da84b6f720f1c0
Austen Lowder <austenswl@gmail.com>
2025-04-07 16:36:00 -0400

    initial code push

---
+++ DemoThreadedFetch/src/App.cs
@@ -0,0 +1,34 @@
+using System.ComponentModel;
+using Microsoft.AspNetCore.Components.Forms;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/wikiApiFetch", (HttpRequest req) => 
+{
+    string? input = req.Query["maxTaskCount"];
+    int maxTaskCount;
+    if (!int.TryParse(input, out maxTaskCount)) {
+        maxTaskCount = 3;
+    }
+    
+    ThreadedFetch data = new ThreadedFetch(maxTaskCount);
+    var apiInfo = data.FetchData();
+    return apiInfo;
+})
+.WithName("GetWikiApiInfo");
+
+app.Run();
---
+++ DemoThreadedFetch/src/requests/ThreadedFetch.cs
@@ -0,0 +1,66 @@
+using Microsoft.AspNetCore.Mvc;
+
+public class ThreadedFetch : ControllerBase
+{
+    public ThreadedFetch(int maxTaskCount) {
+        maxCount = maxTaskCount;
+    }   
+    private static int maxCount;
+    private static readonly HttpClient client = new HttpClient();
+    private static readonly SemaphoreSlim semaphore = new SemaphoreSlim(maxCount);
+    public async Task<IActionResult> FetchData() {
+        var counter = 1;
+        var urls = new List<string>
+        {
+            "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&titles=frank_zappa&rvprop=timestamp|user&rvlimit=27&redirects",
+            "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&titles=frank&rvprop=timestamp|user&rvlimit=27&redirects",
+            "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&titles=hotdog&rvprop=timestamp|user&rvlimit=27&redirects"
+        };
+
+        var tasks = new List<Task<IActionResult>>();
+
+        foreach (var url in urls) {
+            tasks.Add(limitRequest(url, counter));
+            counter += 1;
+        }
+        IActionResult[] responses = await Task.WhenAll(tasks);
+
+        var jsonStrings = responses.OfType<OkObjectResult>().ToList();
+
+        return Ok(jsonStrings);
+    }
+
+    private async Task<IActionResult> limitRequest(string url, int counter){
+        await semaphore.WaitAsync();
+        try{
+            Console.WriteLine($"Task{counter}");
+            var response = await client.GetAsync(url);
+            if(response.IsSuccessStatusCode) {
+                var content = new {
+                    data = new{
+                        rewrites = await response.Content.ReadAsStringAsync(),
+                        task = counter,
+                        startTime = DateTime.UtcNow,
+                        finishTime = DateTime.UtcNow
+                        }
+                };
+                Console.WriteLine($"Task{counter} finsihed");
+                return Ok(content);
+            } else {
+                var content = new {
+                    data = new{
+                        failure = "Failure",
+                        task = counter,
+                        startTime = DateTime.UtcNow,
+                        finishTime = DateTime.UtcNow
+                        }
+                };
+                Console.WriteLine($"Task{counter} finished");
+                return Ok(content);
+            }
+        }
+        finally {
+            semaphore.Release();
+        }
+    }
+}
